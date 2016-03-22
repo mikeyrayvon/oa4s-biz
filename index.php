798544ce08ef7082452e5c7fbd5bea30bbb55f21
Michael Ray-Von <mikey@interglobal.vision>
2016-03-22 10:34:08 -0600

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content" class="container">
+
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+
+</main>
+
+<?php
+get_footer();
+?>
